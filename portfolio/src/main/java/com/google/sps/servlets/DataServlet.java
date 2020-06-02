4b4f4f829655d8b4f16ec1c5b1cd27276037a2ec
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-06-02 16:49:24 -0700

    Add basic comment section

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,18 +26,27 @@ import java.util.ArrayList;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<String> comments = new ArrayList<String>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    ArrayList<String> messages = new ArrayList<String>();
-    messages.add("Hey!");
-    messages.add("Hi!");
-    messages.add("Hello!");
-
     // Convert the server stats to JSON
-    String json = convertToJson(messages);
+    String json = convertToJson(comments);
 
     response.setContentType("application/json;");
     response.getWriter().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {    
+    // Get the input from the form.
+    String text = getParameter(request, "user-comment", "");
+
+    // Add to the array of comments.
+    comments.add(text);
+
+    // Redirect back to the HTML page.
+    response.sendRedirect("/index.html");
   }
 
   /**
@@ -48,5 +57,16 @@ public class DataServlet extends HttpServlet {
     String json = gson.toJson(messages);
     return json;
   }
-}
 
+  /**
+   * @return the request parameter, or the default value if the parameter
+   *         was not specified by the client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+}
